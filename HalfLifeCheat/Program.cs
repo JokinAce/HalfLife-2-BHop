6615e2d54f3d694245f7e6f8ad4d3a6ef686061c
JokinAce <jokinace@pornhubstaff.com>
2021-04-07 19:07:13 +0200

    Projektdateien hinzufügen.

---
+++ HalfLifeCheat/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace HalfLifeCheat {
+
+    internal static class Program {
+
+        private static void Main() {
+            Rewrite rewrite = new Rewrite("hl2");
+
+
+            Console.Title = "HL2 Bhop Cheat | github.com/JokinAce";
+            Console.WriteLine("Half-Life 2 Cheat | github.com/JokinAce\n----------------------------\n1. Half-Life 2\n2. Half-Life 2 EP.1\n3. Half-Life 2 EP.2\n----------------------------\nChoose a Game (Number):");
+
+            string Number = Console.ReadLine();
+
+            if (Number == "1" && rewrite.CheckProcess()) {
+                // ForceJump = client.dll + 48BF5C
+                // Flags = "server.dll"+00634174, 380 Host only <- Shit, not updating fast enough
+
+                int ClientDLL = (int)rewrite.GetDLL("client.dll");
+                FinalMSG(ClientDLL);
+
+                while (true) {
+                    if (rewrite.IsKeyPushedDown(System.Windows.Forms.Keys.Space)) {
+                        rewrite.WriteInt(ClientDLL + 0x48BF5C, 5);
+                        rewrite.WriteInt(ClientDLL + 0x48BF5C, 4);
+                    }
+                }
+            } else if (Number == "2" && rewrite.CheckProcess()) {
+                // ForceJump = client.dll + 4993DC
+                int ClientDLL = (int)rewrite.GetDLL("client.dll");
+                FinalMSG(ClientDLL);
+
+                while (true) {
+                    if (rewrite.IsKeyPushedDown(System.Windows.Forms.Keys.Space)) {
+                        rewrite.WriteInt(ClientDLL + 0x4993DC, 5);
+                        rewrite.WriteInt(ClientDLL + 0x4993DC, 4);
+                    }
+                }
+            } else if (Number == "3") {
+                // FJ = client.dll+4993DC
+                int ClientDLL = (int)rewrite.GetDLL("client.dll");
+                FinalMSG(ClientDLL);
+
+                while (true) {
+                    if (rewrite.IsKeyPushedDown(System.Windows.Forms.Keys.Space)) {
+                        rewrite.WriteInt(ClientDLL + 0x4993DC, 5);
+                        rewrite.WriteInt(ClientDLL + 0x4993DC, 4);
+                    }
+                }
+            }
+
+            void FinalMSG(int ClientDLL) {
+                Console.WriteLine($"ClientDLL - 0x{ClientDLL:X}");
+                Console.WriteLine("Happy Jumping");
+            }
+        }
+    }
+}
---
+++ HalfLifeCheat/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Half-Life 2 Cheat")]
+[assembly: AssemblyDescription("BHop Cheat for HL2 EP.1/2")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AlphaWolf")]
+[assembly: AssemblyProduct("HalfLifeCheat")]
+[assembly: AssemblyCopyright("Copyright © JokinAce 2021")]
+[assembly: AssemblyTrademark("AlphaWolf")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("eac17977-0c9b-48bd-baff-8a4072bb8b89")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
